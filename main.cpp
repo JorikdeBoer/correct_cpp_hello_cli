df1a4988ee88d20606a79b8aeaf54e66d90f5428
Modanung <frode@lindeijer.nl>
2018-01-04 21:50:56 +0100

    hello nice arguments 2

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@ int main(int argc, char* argv[])
 {
   if (argc==1)
   {
-    std::cout << "Hello\n";
+     std::cout << "Hello\n";
   }
   else
   {
-    std::cout << "Hello" << " " << argv[1] << '\n';
+     std::cout << "Hello" << " " << argv[1] << '\n';
   }
 }
